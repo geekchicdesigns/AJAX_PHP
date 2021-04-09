137d7ac910128b12408e9cb79e2143dccf9c0854
geekchiq <31285405+geekchicdesigns@users.noreply.github.com>
2021-04-09 14:33:31 -0400

    update files

---
+++ AJAX/process.php
@@ -0,0 +1,24 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', 'CHarli123!@#', 'ajaxtest');
+
+echo 'Processing...';
+
+if(isset($_POST['name'])){
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    echo 'POST: Your name is '. $_POST['name'];
+
+    $query = "INSERT INTO users(name) VALUES('$name')";
+
+    if(mysqli_query($conn, $query)){
+        echo '  User Added...';
+    } else {
+        echo 'ERROR: '. mysqli_error($conn);
+    }
+}
+
+if(isset($_GET['name'])){
+    echo 'GET: Your name is '. $_GET['name'];
+}
+
+?>
